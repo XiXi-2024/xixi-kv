68aeddb571d23583cf0b4ecad9b9e6e396bcdad9
xixi2024 <15797886724@163.com>
2025-01-23 01:15:39 +0800

    简单实现 List 数据类型

--- datatype/types_test.go
+++ datatype/types_test.go
@@ -182,3 +182,59 @@ func TestDataTypeService_SRem(t *testing.T) {
 	assert.Nil(t, err)
 	assert.False(t, ok)
 }
+
+func TestDataTypeService_LPop(t *testing.T) {
+	opts := bitcask.DefaultOptions
+	dir, _ := os.MkdirTemp("", "bitcask-go-redis-lpop")
+	opts.DirPath = dir
+	dts, err := NewDataTypeService(opts)
+	assert.Nil(t, err)
+
+	res, err := dts.LPush(utils.GetTestKey(1), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.Equal(t, uint32(1), res)
+	res, err = dts.LPush(utils.GetTestKey(1), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.Equal(t, uint32(2), res)
+	res, err = dts.LPush(utils.GetTestKey(1), []byte("val-2"))
+	assert.Nil(t, err)
+	assert.Equal(t, uint32(3), res)
+
+	val, err := dts.LPop(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.NotNil(t, val)
+	val, err = dts.LPop(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.NotNil(t, val)
+	val, err = dts.LPop(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.NotNil(t, val)
+}
+
+func TestDataTypeService_RPop(t *testing.T) {
+	opts := bitcask.DefaultOptions
+	dir, _ := os.MkdirTemp("", "bitcask-go-redis-rpop")
+	opts.DirPath = dir
+	dts, err := NewDataTypeService(opts)
+	assert.Nil(t, err)
+
+	res, err := dts.RPush(utils.GetTestKey(1), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.Equal(t, uint32(1), res)
+	res, err = dts.RPush(utils.GetTestKey(1), []byte("val-1"))
+	assert.Nil(t, err)
+	assert.Equal(t, uint32(2), res)
+	res, err = dts.RPush(utils.GetTestKey(1), []byte("val-2"))
+	assert.Nil(t, err)
+	assert.Equal(t, uint32(3), res)
+
+	val, err := dts.RPop(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.NotNil(t, val)
+	val, err = dts.RPop(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.NotNil(t, val)
+	val, err = dts.RPop(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.NotNil(t, val)
+}
